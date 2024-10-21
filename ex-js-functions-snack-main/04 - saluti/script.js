6ebf4c498aa285d63cb2da17517c780d20039a28
LeonardoR998 <leonardoricci839@gmail.com>
2024-10-21 15:02:47 +0200

    add solution snack 4

--- ex-js-functions-snack-main/04 - saluti/script.js
+++ ex-js-functions-snack-main/04 - saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+function createGreeting(personName) {
+  return `Ciao ${personName}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const greetingMessage = createGreeting(userName);
+console.log(greetingMessage);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
