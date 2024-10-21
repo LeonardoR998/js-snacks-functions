e8130ce84b3930408270dc08f1e43ea364caafba
LeonardoR998 <leonardoricci839@gmail.com>
2024-10-21 15:59:49 +0200

    fix snack 4, name is deprecated

--- ex-js-functions-snack-main/04 - saluti/script.js
+++ ex-js-functions-snack-main/04 - saluti/script.js
@@ -1,15 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = "Mario";
+const nome = "Mario";
 
 // Dichiara la funzione qui.
-function createGreeting(personName) {
-  return `Ciao ${personName}`;
+
+function saluta(nome) {
+  return `Ciao ${nome}`;
 }
 
-// Invoca la funzione qui e stampa il risultato in console
+console.log(saluta(nome));
 
-const greetingMessage = createGreeting(userName);
-console.log(greetingMessage);
+// Invoca la funzione qui e stampa il risultato in console
+console.log(saluta(nome));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
